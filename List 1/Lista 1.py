c89c6783e1e17ebb27e6eac380d2432a92aaa782
Kacper Budnik <84456252+KacperBudnik@users.noreply.github.com>
2021-10-11 22:11:11 +0200

    Update Lista 1.py

--- List 1/Lista 1.py
+++ List 1/Lista 1.py
@@ -1,12 +1,16 @@
 def NWD(a,b):
     while a!=b:
-        print(a,b)
+        #print(a,b)
         if a>b:
             if b==1:
                 a=1
             else:   
                 if a>b*10**3:
-                    a-=b*10**3
+                    p=4
+                    while a>b*10**p:
+                        p+=1
+                    p-=1
+                    a-=b*10**p
                 else:
                     a-=b
         else:
@@ -14,7 +18,11 @@ def NWD(a,b):
                 b=1
             else:
                 if b>a*10**3:
-                    b-=a*10**3
+                    p=4
+                    while b>a*10**p:
+                        p+=1
+                    p-=1
+                    b-=a*10**p
                 else:
                     b-=a
     return a
@@ -22,7 +30,6 @@ def NWD(a,b):
 
 class Fraction:
     def __init__(self, Num, Dem):
-        print(type(Num), type(Dem))
         if type(Num)!=int or type(Dem)!= int:
             raise ValueError("Muszą być liczby całkowite")
         
@@ -31,7 +38,6 @@ class Fraction:
         
         nwd=NWD(abs(Num),abs(Dem))
         self.sign = 1 if Num*Dem>0 else -1 if Num*Dem<0 else 0
-        print(self.sign)
         self.num=abs(Num)//nwd
         self.dem=abs(Dem)//nwd
         
@@ -128,8 +134,11 @@ class frac:
             Num=int(abs(Num*10**frac.precision))
             Dem=int(abs(Dem*10**frac.precision))
 
-        
-        nwd=NWD(Num,Dem)
+        if Num!=0:
+            nwd=NWD(Num,Dem)
+        else:
+            nwd=1
+            Dem=1
         self.num=Num//nwd
         self.dem=Dem//nwd
         
@@ -165,20 +174,40 @@ class frac:
         return self * other**(-1)
         
     def __gt__(self,other):
+        if type(other)!=frac:
+            other=frac(other)
         if self.sign*self.num*other.dem > other.sign*other.num*self.dem:
             return True
         return False
     
     def __ge__(self,other):
+        if type(other)!=frac:
+            other=frac(other)
         if self.sign*self.num*other.dem >= other.sign*other.num*self.dem:
             return True
         return False
     
     def __eq__(self, other):
+        if type(other)!=frac:
+            other=frac(other)
         if self.sign*self.num*other.dem == other.sign*other.num*self.dem:
             return True
         return False
-                
+    
+    def __lt__(self,other):
+        if type(other)!=frac:
+            other=frac(other)
+        if self.sign*self.num*other.dem < other.sign*other.num*self.dem:
+            return True
+        return False
+    
+    def __le__(self,other):
+        if type(other)!=frac:
+            other=frac(other)
+        if self.sign*self.num*other.dem <= other.sign*other.num*self.dem:
+            return True
+        return False
+            
     def getNum(self):
         return self.num
     
@@ -194,11 +223,70 @@ class frac:
             a = self.num//self.dem
             b = self.num - a*self.dem
             return str(self.sign*a) + " i " + str(b) + " // " + str(self.dem) if b !=0 else str(self.sign*a)
+        
+    def __repr__(self):
+        return self.__str__()
 
     def show_mixed():
         frac.mixed=not frac.mixed
+        
+    def __abs__(self):
+        return frac(self*self.sign)
+    
+    def as_integer_ratio(self):
+        return (self.sign*self.num, self.dem)
+    
+    def __neg__(self):
+        return frac((-1)*self)
+    
+    def __pos__(self):
+        return frac(self)
+    
         
 #frac.precision=2
 #a=frac(2,3)
 #b=frac(1,2)
 #b*2
+
+"""
+import math
+a=frac(1,2)
+b=frac(1,3)
+c=frac(-1/2)
+f=frac(math.pi)
+
+a+b
+a-b
+a*b
+a+c
+a-c
+a*5
+frac.mixed=True
+a*5
+f
+frac.decimal=True
+f
+h=a**b
+h
+h=h**3
+h
+h-a
+a-1/2
+frac(1/4)**a
+frac.decimal=False
+frac.mixed=False
+a=frac(1,1)
+a
+a/2
+a>1/2
+a/2>1/2
+a/2>=1/2
+1/2>=a/2
+a/2>=frac(1/3)
+1==a
+
+frac.decimal=True
+k=-f
+k
+k+f
+"""

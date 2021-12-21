cd82e06001ac77eb7784f2f4e705f15e5826c8c8
Kacper Budnik <84456252+KacperBudnik@users.noreply.github.com>
2021-12-21 15:59:59 +0100

    Update

--- List 5/computational_complexity.py
+++ List 5/computational_complexity.py
@@ -15,20 +15,13 @@ def solve_n_time(n,a=-128,b=128,times=1):
     return timeit.timeit("linalg.solve(A,b)","from scipy import linalg",number=times,globals=locals())
 
 
-
-
-plt.plot(range(15), [np.log2(i) for i in times])
-plt.yscale=('log')
-plt.show()
-
-
 def curve(x,a,b,n):
     return a*x**n+b
 
-def n(m):
-
+def n(m=15,tim=1):
+    
     times=np.zeros(m)
     for i in range(m):
-        times[i]=solve_n_time(2**i,times=1)
+        times[i]=solve_n_time(2**i,times=tim)
         
     return fit(curve,[2**i for i in range(m)], times)[0][2] # = 2.77 za pierwszym, 2.24 za drugim
